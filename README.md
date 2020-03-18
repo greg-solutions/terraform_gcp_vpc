0555375cb50c9387897b2774ebfe5cce65146f77
vadim.didenko <vadim.didenko@greg-solutions.com>
2020-03-18 12:43:09 +0200

    Init

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-# Terraform Google Cloud Kubernetes Engine
-
-This Terraform module provisions a regional Google Kubernetes Engine (GKE) cluster
-
+# Terraform
--- data.tf
+++
@@ -1,6 +0,0 @@
-data "google_container_engine_versions" "cluster_engine_version" {
-  version_prefix = "latest"
-}
-
-data "google_client_config" "config" {
-}
