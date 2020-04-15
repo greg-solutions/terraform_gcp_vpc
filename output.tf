052f18acbfa6fede0199ee8689bea038e16cb318
bohdantverdyi <ownedd777@gmail.com>
2020-04-15 18:02:35 +0300

    Add output self link for netowrk

--- output.tf
+++ output.tf
@@ -7,3 +7,6 @@ output "subnetworks_name" {
 output "ip_cidr_ranges" {
   value = google_compute_subnetwork.public_subnet.*.ip_cidr_range
 }
+output "network_self_link" {
+  value = google_compute_network.vpc_network.self_link
+}
