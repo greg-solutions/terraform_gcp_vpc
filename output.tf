0fe2f7ac6fd9f424de588b7bae2453aec177d2a7
bohdantverdyi <ownedd777@gmail.com>
2020-07-23 14:27:09 +0300

    add network id output

--- output.tf
+++ output.tf
@@ -10,3 +10,6 @@ output "ip_cidr_ranges" {
 output "network_self_link" {
   value = google_compute_network.vpc_network.self_link
 }
+output "network_id" {
+  value = google_compute_network.vpc_network.id
+}
